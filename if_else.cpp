2e1e21f6ce0f3be81e822ff771bcc2cde843fc0a
sayadfdali <sayadfdali@gmail.com>
2025-12-13 05:36:54 +0600

    Shortcut loop for c++

--- if_else.cpp
+++ if_else.cpp
@@ -3,13 +3,7 @@ using namespace std;
 int main()
 {
     int x =6;
-    if(x%2==0)
-    {
-        cout << "Even\n";
-    }
-    else
-    {
-        cout<< "Odd\n";
-    }
+    x%2==0 ? cout << "Even\n" : cout << "Odd\n";
+
     return 0;
 }
