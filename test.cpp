3791fc6a29828d886eab3d2a4e4bf08e10205e1d
sayadfdali <sayadfdali@gmail.com>
2025-12-13 02:35:17 +0600

    basic practice of c++

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    int x = 10;
+    char c= 'A';
+    double d = 45.5;
+
+    std :: cout <<x << " " <<c << std :: endl <<d <<std :: endl;
+    std :: cout << "My favorite nunber is " << x << std :: endl;
+    return 0;
+}
