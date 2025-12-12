c2dd903d2377257c001b640ab8a5c24f0d2fa8ee
sayadfdali <sayadfdali@gmail.com>
2025-12-13 05:21:14 +0600

    setprecision practice

--- input.cpp
+++ input.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main() 
 {
     int x;
-    cin >>x;
-    cout << x << endl;
+    cin >>x ;
+    cout << x  << endl;
+    char c = 'A';
+    cout << (int)c << endl;
     return 0;
 }
